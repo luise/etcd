15a07779f06379afe870681cd787f9995d30dcd1
Luise Valentin <valentin.luise@gmail.com>
2017-02-17 21:07:41 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Etcd for Quilt.js
+
+This repository implements an Etcd specification for Quilt.js.  See
+[Quilt](http://quilt.io) for more information.
---
+++ etcd-example.js
@@ -0,0 +1,14 @@
+var etcd = require("github.com/quilt/etcd");
+
+var nWorker = 3;
+
+var deployment = createDeployment({});
+
+var baseMachine = new Machine({
+    provider: "Amazon",
+    sshKeys: githubKeys("ejj"), // Replace with your GitHub username.
+});
+
+deployment.deploy(baseMachine.asMaster())
+deployment.deploy(baseMachine.asWorker().replicate(nWorker + 1))
+deployment.deploy(new etcd.Etcd(nWorker));
