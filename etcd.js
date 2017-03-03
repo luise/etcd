e1a342e939e0ebbffca72f3bc8145f1ed5e3c2fb
Kevin Lin <kaikai526@gmail.com>
2017-03-02 18:10:28 -0600

    lib: Remove custom Quilt image
    This patch removes the custom Quilt image in favor of creating the
    environment variables from within the spec. This is also possible
    because containers no longer need to block on name resolution due to
    improvements in the QRI network implementation.

--- etcd.js
+++ etcd.js
@@ -1,14 +1,24 @@
-var image = "quilt/etcd";
-
 function Etcd(n) {
-    var refContainer = new Container(image);
+    var refContainer = new Container("quay.io/coreos/etcd:v3.0.2");
     this.etcd = new Service("etcd", refContainer.replicate(n));
     var children = this.etcd.children();
-    var peers = children.join(",");
-    for (var i = 0; i < this.etcd.containers.length; i++) {
-        this.etcd.containers[i].setEnv("PEERS", peers);
-        this.etcd.containers[i].setEnv("HOST", children[i]);
-    }
+
+    var initialCluster = [];
+    children.forEach(function(host) {
+        initialCluster.push(host + "=http://" + host + ":2380");
+    });
+    var initialClusterStr = initialCluster.join(",");
+
+    this.etcd.containers.forEach(function(c, i) {
+        var host = children[i];
+        c.setEnv("ETCD_NAME", host);
+        c.setEnv("ETCD_LISTEN_PEER_URLS", "http://" + host + ":2380");
+        c.setEnv("ETCD_LISTEN_CLIENT_URLS", "http://0.0.0.0:2379");
+        c.setEnv("ETCD_INITIAL_ADVERTISE_PEER_URLS", "http://" + host + ":2380");
+        c.setEnv("ETCD_INITIAL_CLUSTER", initialClusterStr);
+        c.setEnv("ETCD_INITIAL_CLUSTER_STATE", "new");
+        c.setEnv("ETCD_ADVERTISE_CLIENT_URLS", "http://" + host + ":2379");
+    })
     this.etcd.connect(new PortRange(1000, 65535), this.etcd);
 
     this.deploy = function(deployment) {
