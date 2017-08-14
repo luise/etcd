b8d3d33e38466d3c20e632f267d8e7148f05eb55
Kevin Lin <kevin@quilt.io>
2017-08-14 16:46:25 -0700

    blueprint: Use new Quilt API
    The Quilt API now deploys containers directly.

--- etcd.js
+++ etcd.js
@@ -1,21 +1,16 @@
-const { Container, Service, PortRange } = require('@quilt/quilt');
+const { Container, PortRange, allow } = require('@quilt/quilt');
 
 function Etcd(n) {
-  const refContainer = new Container('quay.io/coreos/etcd:v3.0.2');
-  this.etcd = new Service('etcd', refContainer.replicate(n));
+  const refContainer = new Container('etcd', 'quay.io/coreos/etcd:v3.0.2');
+  this.cluster = refContainer.replicate(n);
 
-  this.etcd.containers.forEach((c, i) => {
-    c.setHostname(`etcd${i}`);
-  });
-
-  const initialCluster = [];
-  this.etcd.containers.forEach((c) => {
+  const initialCluster = this.cluster.map((c) => {
     const host = c.getHostname();
-    initialCluster.push(`${host}=http://${host}:2380`);
+    return `${host}=http://${host}:2380`;
   });
   const initialClusterStr = initialCluster.join(',');
 
-  this.etcd.containers.forEach((c) => {
+  this.cluster.forEach((c) => {
     const host = c.getHostname();
     c.setEnv('ETCD_NAME', host);
     c.setEnv('ETCD_LISTEN_PEER_URLS', `http://${host}:2380`);
@@ -25,10 +20,10 @@ function Etcd(n) {
     c.setEnv('ETCD_INITIAL_CLUSTER_STATE', 'new');
     c.setEnv('ETCD_ADVERTISE_CLIENT_URLS', `http://${host}:2379`);
   });
-  this.etcd.allowFrom(this.etcd, new PortRange(1000, 65535));
+  allow(this.cluster, this.cluster, new PortRange(1000, 65535));
 
   this.deploy = function deploy(deployment) {
-    deployment.deploy(this.etcd);
+    deployment.deploy(this.cluster);
   };
 }
 
