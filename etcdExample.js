269340feedce1f92f37d0e2c9e102fc0db612295
Luise Valentin <luise@kelda.io>
2017-10-27 14:03:03 -0700

    blueprint: Remove sshKeys Machine property
    Kelda now auto generates ssh keys for machines, so most users won't need to
    use the `sshKeys` Machine property.

--- etcdExample.js
+++ etcdExample.js
@@ -1,12 +1,9 @@
-const { Infrastructure, githubKeys, Machine } = require('kelda');
+const { Infrastructure, Machine } = require('kelda');
 const etcd = require('./etcd.js');
 
 const nWorker = 3;
 
-const baseMachine = new Machine({
-  provider: 'Amazon',
-  sshKeys: githubKeys('ejj'), // Replace with your GitHub username.
-});
+const baseMachine = new Machine({ provider: 'Amazon' });
 const infra = new Infrastructure(baseMachine, baseMachine.replicate(nWorker));
 
 const etcdApp = new etcd.Etcd(nWorker);
